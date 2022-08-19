b0f72054053470d81068289f5d512ced836b3644
이다은 <llynn97@naver.com>
2022-08-19 12:44:50 +0900

    220819

--- 백준/15685.cpp
+++ 백준/15685.cpp
@@ -14,13 +14,17 @@ int dy[4] = { 1,0,-1,0 };// 오른 , 위, 왼 , 아래
 int dx[4] = { 0,-1,0,1 };
 vector <int> v;
 vector <int> v2;
-
+/*
+  0 1
+  0 1 2 1
+  0 1 2 1 2 3 2 1
+*/
 
 void check(int x, int y, int d, int g) {
 	int tx = x;
 	int ty = y;
 	v.push_back(d);
-	if (g>= 1) {
+	if (g >= 1) {
 		for (int i = 1; i <= g; i++) {
 			for (int j = v.size() - 1; j >= 0; j--) {
 				v2.push_back(v[j]);
@@ -28,34 +32,33 @@ void check(int x, int y, int d, int g) {
 			for (int j = 0; j < v2.size(); j++) {
 				v.push_back((v2[j] + 1) % 4);
 			}
-			
+
 			v2.clear();
 		}
 	}
-	
+
 	map[ty][tx] = 1;
 	for (int i = 0; i < v.size(); i++) {
 		int ny = ty + dy[v[i]];
 		int nx = tx + dx[v[i]];
 		map[ny][nx] = 1;
-		
 		tx = nx;
 		ty = ny;
 	}
 	v.clear();
 	v2.clear();
-	
+
 }
 
 int countSquare() {
 	int cnt = 0;
 	for (int i = 0; i < 101; i++) {
 		for (int j = 0; j < 101; j++) {
-			if (map[i][j] == 1 && map[i+1][j] == 1&& map[i][j+1]==1 && map[i+1][j+1]==1) {
+			if (map[i][j] == 1 && map[i + 1][j] == 1 && map[i][j + 1] == 1 && map[i + 1][j + 1] == 1) {
 				cnt++;
 			}
 		}
-		
+
 	}
 	return cnt;
 }
@@ -68,9 +71,8 @@ int main(void) {
 
 	}
 
-	
 
-	cout<<countSquare();
+	cout << countSquare();
 
-	
+
 }
